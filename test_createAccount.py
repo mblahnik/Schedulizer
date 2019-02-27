d2aac1399f147321c96ad62d760cd63675af3181
Alex <mblahnik@uwm.edu>
2019-02-27 13:45:15 -0600

    Working on some stuff

--- test_createAccount.py
+++ test_createAccount.py
@@ -3,14 +3,31 @@ from unittest import TestCase
 
 class TestCreateAccount(TestCase):
 
+    def setup(self):
 
-    def test_command_create_success(self):
-        self.assertEquals(self.ui.command("createAccount TA"), "TA account successfully created")
-        
+      """
+         When the createAccount command is entered, it takes two arguments:
+         -Account name
+         -title
+         If the account being created already exists in the database an error message is displayed
+      """
+    def test_command_createAccount_success(self):
+        self.assertEquals(self.ui.command("createAccount accountName  title"), "Account successfully created")
 
-    def test_command_create_fail(self):
+    def test_command_createAccount_no_title(self):
+        self.assertEquals(self.ui.command("createAccount accountName "), "Need to specify a title")
+
+    def test_command_createAccount_no_name(self):
+        self.assertEquals(self.ui.command("createAccount title "), "Need to specify a name")
+
+    def test_command_createAccount_no_args(self):
+        self.assertEquals(self.ui.command("createAccount title "), "Please enter a name and title")
+
+    def test_command_createAccount_already_exists(self):
+        self.assertEquals(self.ui.command("createAccount accountName title "), "Account already exists")
 
 
-    def test_command_creat_no_args(self):
+
+
 
 
