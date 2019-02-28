60a619ff8f8b1467074740b54da4993d91adddc1
spykim2003 <47366411+spykim2003@users.noreply.github.com>
2019-02-28 11:16:28 -0600

    Update test_viewCourseAssignments.py

--- test_viewCourseAssignments.py
+++ test_viewCourseAssignments.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-class viewCourseAssignments(TestCase):
+class test_viewCourseAssignments(TestCase):
 
 
   def setup(self):
@@ -8,9 +8,10 @@ class viewCourseAssignments(TestCase):
    """
    When the user type the command viewCourseAssignments
    User need to enter the password to see the schedule
-    For search the the assignments from other professors
+    For searching the the assignments from other professors
     The command for view my schedule :viewMySchedule
     Takes 2 argument for search
+    search userName
    """
 
 def test_command_password_was_correct
@@ -19,15 +20,16 @@ def test_command_password_was_correct
 
 def test_command_password_was_incorrect
     self.assertEquals(self.ui.command("password"), "Password is incorrected, there are 3 more chances to type it")
+
 def test_command_password_was_incorrect_3times
     self.assertEquals(self.ui.command("password"), "Passwrod is incorrected for 3 times contact to administrator")
 
 def test_command_can_not_view_my_schedule
-        self.assertEquals(self.ui.command("viewMySchedule"), "The schedule hasn't been uploaded yet")
+    self.assertEquals(self.ui.command("viewMySchedule"), "The schedule hasn't been uploaded yet")
 
 
 def test_command_cannot_find_name
-        self.assertEquals(self.ui.command("search userName"), "Can't fine the name of the professor, retype it")
+    self.assertEquals(self.ui.command("search userName"), "Can't fine the name of the professor, retype it")
 
 
 def test_command_username_not_typed
