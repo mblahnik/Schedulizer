6b24c1c93188664b65eda91f745c2c63affec635
spykim2003 <47366411+spykim2003@users.noreply.github.com>
2019-02-28 12:29:59 -0600

    commit

---
+++ test_assignInstructorClass.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+class test_assignInstructorClass(TestCase):
+
+    def setUp(self):
+        self.ui.command("createAccount accountName")
+
+
+"""
+  Type numOfAssigned to check how many classes an instructor is currently assigned to
+  type viewSchedule to see the availability schedule
+
+    To assign the class; it takes 3 arguments
+    assign className classNumber
+   """
+
+def test_command_password_was_correct
+       self.assertEquals(self.ui.command("password"), "You have just entered sendOutNotification system")
+
+
+def test_command_password_was_incorrect
+    self.assertEquals(self.ui.command("password"), "Password is incorrect, there are 3 more chances to type it")
+
+def test_command_can_not_view_schedule
+    self.assertEquals(self.ui.command("viewSchedule"), "The schedule hasn't been uploaded yet")
+
+def test_command_can_not_view_numOfAssigned
+    self.assertEquals(self.ui.command("numOfAssigned"), "Can't view number of classed that are assigned to you")
+
+
+
+def test_command_class_number_invalid
+    self.assertEquals(self.ui.command("assign className"), "type the class number")
+
+def test_command_class_name_invalid
+    self.assertEquals(self.ui.command("assign classNumber"), "type the class number")
+
+
+def test_command_class_name_number_invalid
+    self.assertEquals(self.ui.command("assign"), "type the class number & name")
+
+def test_command_conflicted_class
+    self.assertEquals(self.ui.command("assign className classNumber"), "This class was already assigned")
+
