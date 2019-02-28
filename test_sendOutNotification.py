9ff1e9c7f43f62da97fc3f742be4c912985fe957
Erik <spykim2003@gmail.com>
2019-02-28 12:57:49 -0600

    commit

--- test_sendOutNotification.py
+++ test_sendOutNotification.py
@@ -25,25 +25,22 @@ to send notification to one person
    """
 
 def test_command_password_was_correct(self):
-       self.assertEquals(self.ui.command("password"), "You have just entered sendOutNotification system")
+    self.assertEquals(self.ui.command("password"), "You have just entered sendOutNotification system")
 
 
 def test_command_password_was_incorrect(self):
     self.assertEquals(self.ui.command("password"), "Password is incorrect, there are 3 more chances to type it")
 
 
- def test_command_nofitication_was_not_sent(self):
+def test_command_notification_was_not_sent(self):
+    self.assertEquals(self.ui.command("sendNotification accountName"), "We weren't able to send a notification")
 
-         self.assertEquals(self.ui.command("sendNotification accountName"), "We weren't able to send a notification")
 
+def test_command_notification_was_not_sent_all(self):
+     self.assertEquals(self.ui.command("sendNotification accountName -a"), "We weren't able to send a notification")
 
- def test_command_nofitication_was_not_sent_all(self):
-
-         self.assertEquals(self.ui.command("sendNotification accountName -a"), "We weren't able to send a notification")
-
- def test_command_nofitication_was_not_sent_specific(self):
-
-         self.assertEquals(self.ui.command("sendNotification accountNames -s"), "We weren't able to send a notification")
+def test_command_notification_was_not_sent_specific(self):
+     self.assertEquals(self.ui.command("sendNotification accountNames -s"), "We weren't able to send a notification")
 
 def test_command_no_argument(self):
-            self.assertEquals(self.ui.command("sendNotification"), "Please type the email that you want to sent")
+    self.assertEquals(self.ui.command("sendNotification"), "Please type the email that you want to sent")
