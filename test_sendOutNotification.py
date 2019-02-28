7bb67e3cce26b9ea8bd925419b1bccd8ad7e1f0b
spykim2003 <47366411+spykim2003@users.noreply.github.com>
2019-02-28 12:41:03 -0600

    Update test_sendOutNotification.py

--- test_sendOutNotification.py
+++ test_sendOutNotification.py
@@ -24,26 +24,26 @@ sendNotification  accountName
 to send notification to one person
    """
 
-def test_command_password_was_correct
+def test_command_password_was_correct(self):
        self.assertEquals(self.ui.command("password"), "You have just entered sendOutNotification system")
 
 
-def test_command_password_was_incorrect
+def test_command_password_was_incorrect(self):
     self.assertEquals(self.ui.command("password"), "Password is incorrect, there are 3 more chances to type it")
 
 
- def test_command_nofitication_was_not_sent
+ def test_command_nofitication_was_not_sent(self):
 
          self.assertEquals(self.ui.command("sendNotification accountName"), "We weren't able to send a notification")
 
 
- def test_command_nofitication_was_not_sent_all
+ def test_command_nofitication_was_not_sent_all(self):
 
          self.assertEquals(self.ui.command("sendNotification accountName -a"), "We weren't able to send a notification")
 
- def test_command_nofitication_was_not_sent_specific
+ def test_command_nofitication_was_not_sent_specific(self):
 
          self.assertEquals(self.ui.command("sendNotification accountNames -s"), "We weren't able to send a notification")
 
-def test_command_no_argument
+def test_command_no_argument(self):
             self.assertEquals(self.ui.command("sendNotification"), "Please type the email that you want to sent")
