daea3085aaf23433fed028aa62b56d5a764e23aa
Alex <mblahnik@uwm.edu>
2019-03-06 14:22:45 -0600

    working in lab

--- instructor.py
+++ instructor.py
@@ -3,7 +3,11 @@ from account import account
 
 class instructor(account):
 
-
     def __init__(self,name,title):
         super().__init__(self,name,title)
-        courseAssignments = []
+        self.courses = []
+        self.accountInfo["Courses"] = self.courses
+
+    def addcourse(self, newCourse):
+        self.accountInfo["Courses"] = self.courses.append(newCourse)
+
