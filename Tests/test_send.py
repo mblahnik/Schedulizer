23ea583c326a758f82d8cca9e1f58def4b132a2f
Erik <spykim2003@gmail.com>
2019-03-12 00:09:14 -0500

    commit

--- Tests/test_send.py
+++ Tests/test_send.py
@@ -4,9 +4,9 @@ from Commands import send
 class test_send(unittest.TestCase):
 
     def setUp(self):
-        self.send1 = test_send(self, "eonshik")
-        self.send2 = test_send(self, "eonshik spykim2003" "-s")
-        self.send3 = test_send(self, "-a")
+        self.send1 = send(self, "eonshik")
+        self.send2 = send(self, "eonshik spykim2003" "-s")
+        self.send3 = send(self, "-a")
 
     def test_name(self):
         self.assertEqual(self.send1.getAccountName(), "eonshik")
