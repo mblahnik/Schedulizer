2d19d6b228894339e802241cf8b80ba8aa91bac6
Erik <spykim2003@gmail.com>
2019-03-12 00:18:10 -0500

    commit

--- Tests/test_send.py
+++ Tests/test_send.py
@@ -4,9 +4,9 @@ from Commands import send
 class test_send(unittest.TestCase):
 
     def setUp(self):
-        self.send1 = send(self, "eonshik")
-        self.send2 = send(self, "eonshik spykim2003" "-s")
-        self.send3 = send(self, "-a")
+        self.send1 = send("eonshik")
+        self.send2 = send("eonshik spykim2003" "-s")
+        self.send3 = send("-a")
 
     def test_name(self):
         self.assertEqual(self.send1.getAccountName(), "eonshik")
@@ -14,10 +14,13 @@ class test_send(unittest.TestCase):
     def test_multiple_names(self):
         self.assertEqual(self.send2.getAccountNames(), "eonshik spykim2003")
 
+    def test_speicific_option(self):
+        with self.assertRaises(ValueError):
 
 
 
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+            if __name__ == '__main__':
+                 unittest.main()
