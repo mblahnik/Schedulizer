4fe81195f5ed1e1e50ae62bc8df116af21be598f
nmscannell <scannell@uwm.edu>
2019-03-11 23:08:14 -0500

    added createLab tests

--- Tests/test_edit.py
+++ Tests/test_edit.py
@@ -1,9 +1,22 @@
 import unittest
+from editHelper import EditHelper
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestEdit(unittest.TestCase):
+    def testNSU(self):
+        self.assertEqual(self.edit("user", "command"), "No such user")
+
+    def testEditHPhone(self):
+        self.assertEqual(self.edit("user", "HPhone", "pho numb"), "Home phone number updated")
+
+    def testEditOPhone(self):
+        self.assertEqual(self.edit("user", "OPhone", "pho numb"), "Office phone number updated")
+
+    def testEditOHours(self):
+        self.assertEqual(self.edit("user", "OHours", "hrs"), "Office hours updated")
+
+    def testEditEmail(self):
+        self.assertEqual(self.edit("user", "Email", "email"), "Email updated.")
 
 
 if __name__ == '__main__':
