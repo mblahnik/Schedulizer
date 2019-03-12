4867ccbb19397e50f70164144c34eee1f90cdb18
Leuenberger <leuenbe4@uwm.edu>
2019-03-11 22:34:07 -0500

    createCourse tests

--- Tests/test_createLab.py
+++ Tests/test_createLab.py
@@ -1,10 +1,34 @@
-import unittest
+from unittest import TestCase
+from Commands import createLab
+from Course import Course
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class test_createLab(TestCase):
+
+    def setUp(self):
+        self.Course1 = Course(361)
+        self.Lab1 = createLab.createLab(self, 361, 101, "W", 1300, 1445 )
+
+    def test_lab_assigned_to_course(self):
+        self.assertTrue(Lab1 in self.Course1.courseInfo["labs"])
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == 'test_createLab':
+    test_createLab()
