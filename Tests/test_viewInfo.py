601832c7e0a31b30be48b2e605b83980e9a69bc6
Alex <mblahnik@uwm.edu>
2019-03-12 21:25:46 -0500

    createAccount and Test_createAccount done

--- Tests/test_viewInfo.py
+++ Tests/test_viewInfo.py
@@ -1,10 +1,22 @@
 import unittest
-
+from Commands import viewInfo
+from instructor import instructor
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.Instructor = instructor()
+        self.Instructor.accountName = "Mr.Sparkle"
+        self.Instructor.accountInfo["Home Phone"] = "555-0113"
+        self.Instructor.accountInfo["Email"] = "chunkylover53@aol.com"
+        self.Instructor.accountInfo["address"] = "123 Fake Street"
+        self.Instructor.accountInfo["Office Phone"] = "555-5555"
+        self.Instructor.accountInfo["Office hours"] = "All day everyday"
 
+    def test_viewInfo(self):
+        self.assertEqual(viewInfo(self.Instructor), "Name: Mr.Sparkle"
+                                                    "Email: chumnkylover53@aol.com"
+                                                    "Office Phone: 555-5555"
+                                                    "Office hours: All day everyday")
 
 if __name__ == '__main__':
     unittest.main()
