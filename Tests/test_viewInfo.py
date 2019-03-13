395d15bfec266a735e85ced87bae1abd959442f7
AdrianBozdog <47398751+AdrianBozdog@users.noreply.github.com>
2019-03-12 21:52:27 -0500

    Add files via upload

--- Tests/test_viewInfo.py
+++ Tests/test_viewInfo.py
@@ -1,22 +1,27 @@
-import unittest
-from Commands import viewInfo
-from instructor import instructor
+from unittest import TestCase
+from account import account
 
-class MyTestCase(unittest.TestCase):
+
+class TestAccount(TestCase):
+
     def setUp(self):
-        self.Instructor = instructor()
-        self.Instructor.accountName = "Mr.Sparkle"
-        self.Instructor.accountInfo["Home Phone"] = "555-0113"
-        self.Instructor.accountInfo["Email"] = "chunkylover53@aol.com"
-        self.Instructor.accountInfo["address"] = "123 Fake Street"
-        self.Instructor.accountInfo["Office Phone"] = "555-5555"
-        self.Instructor.accountInfo["Office hours"] = "All day everyday"
+        self.Account1 = account()
+        self.Account1.accountName = "Mr.Sparkle"
+        self.Account1.accountInfo["Home Phone"] = "555-0113"
+        self.Account1.accountInfo["Email"] = "chunkylover53@aol.com"
+        self.Account1.accountInfo["address"] = "123 Fake Street"
+        self.Account1.accountInfo["Office Phone"] = "555-5555"
+        self.Account1.accountInfo["Office hours"] = "All day everyday"
 
-    def test_viewInfo(self):
-        self.assertEqual(viewInfo(self.Instructor), "Name: Mr.Sparkle"
-                                                    "Email: chumnkylover53@aol.com"
-                                                    "Office Phone: 555-5555"
-                                                    "Office hours: All day everyday")
+
+    def test_viewPersonal:
+        self.assertEqual(Account1.viewName(),"Mr.Sparkle")
+        self.assertEqual(Account1.viewHomePhone(), "Not public info")
+        self.assertEqual(Account1.viewOfficePhone(), "555-5555")
+        self.assertEqual(Account1.viewAdress(), "Not public info")
+        self.assertEqual(Account1.viewEmail(), "chunkylover53@alo.com")
+        self.assertEqual(Account1.viewOfficeHours, "All day everyday")
+
 
 if __name__ == '__main__':
     unittest.main()
